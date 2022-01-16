593c89a4dfced926c6c10e44fa9c212aa4b467ed
jynrock <77015465+jynrock@users.noreply.github.com>
2022-01-16 18:57:38 +0100

    Update and rename nuage2.py to cloud.py

---
+++ Nuage-randomwalk/cloud.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random as random
+
+i = 0
+
+while i < 15:
+	n = random.randint(2000, 50000)
+	m = random.randint(5, 100)
+	def points(n=n):
+	    x = np.random.uniform(size=n)
+	    y = np.random.uniform(size=n)
+	
+	    return x, y
+	x, y = points()
+	
+	
+	fig = plt.figure(figsize=(15,5))
+	
+	plt.scatter(x, y, c=np.random.rand(len(x)), s=m, cmap='rainbow', alpha=0.6)
+	plt.axis('off')
+	plt.savefig("Clouds/Cloud_"+str(n)+"-"+str(m)+".png")
+	i = i+1
